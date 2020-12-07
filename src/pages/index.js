a0b64e620ca07910a829905c0f9c022e86f3c68f
Mandi <haase1020@gmail.com>
2020-12-07 06:10:15 -0500

    working on blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,60 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
+
+export default ({data }) => {
+  const {
+    allStrapiProjects:{nodes:projects},
+    allStrapiBlogs:{nodes:blogs}
+  } = data
   return <Layout>
     <Hero />
     <Services />
     <Jobs />
+    <Projects projects={projects} title="featured projects" showLink/>
+    <Blogs blogs={blogs} title="blog" showLink/>
   </Layout>
 }
-// ...GatsbyImageSharpFluid
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: {featured: {eq: true}}) {
+      nodes {
+        description
+        github
+        id
+        url
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+      nodes {
+        slug
+        content
+        date(formatString: "MMMM Do, YYYY")
+        desc
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,44 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
-// ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+
+const ProjectsPage = ({
+  data:{allStrapiProjects:{nodes:projects}}
+}) => {
+  return <layout>
+    <section className="projects-page">
+      <Projects projects={projects} title="all projects" />
+    </section>
+  </layout>
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        description
+        github
+        id
+        url
+        title
+        image {
+          childImageSharp {
+            fluid {
+               ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
+
+
+
 
 export default ProjectsPage
